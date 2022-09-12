ad54655af4d6e90c33b56b9dbdb8d8484972ceb6
sbnn3 <sebastian.neagu@outlook.com>
2022-09-12 13:47:37 +0000

    Fixed the allowed hosts in settings

--- funny_blog/settings.py
+++ funny_blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sbnn-funny-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['sbnn3-funny-blog.herokuapp.com', 'localhost']
 
 
 # Application definition
