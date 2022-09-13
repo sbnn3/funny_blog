ed2eb3d574f66cb4e442b8f3860da16e54c34373
sbnn3 <sebastian.neagu@outlook.com>
2022-09-13 16:15:35 +0000

    Create models for Guitars section and added to admin

--- guitars/models.py
+++ guitars/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Guitars(models.Model):
+
+    guitar_model = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    slug = models.SlugField(max_length=50, unique=True, null=False, blank=False)
+    artist = models.ForeignKey(User, on_delete=models.CASCADE, related_name='guitars')
+    description = models.TextField()
+    guitar_image = CloudinaryField('image', null=False, blank=False)
+    remaining_guitars = models.IntegerField(null=False, blank=False)
+    price = models.IntegerField(null=False, blank=False)
+    likes = models.ManyToManyField(User, related_name='guitar_likes', blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.guitar_model
+
+    def number_of_likes(self):
+        return self.likes.count()
