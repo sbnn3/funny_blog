1a20b70339769d85807537fc9f4b31e7c3f945b1
sbnn3 <sebastian.neagu@outlook.com>
2022-09-13 17:10:47 +0000

    Added views and tried an url that wasnt work, also by mistake ive deleted the urls.py general one and recovered back

--- guitars/views.py
+++ guitars/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Guitars
 
-# Create your views here.
+class GuitarsPage(generic.ListView):
+    model = Guitars
+    queryset = Guitars.objects.filter(status=1).order_by('-created_on')
+    template_name = 'guitars.html'
+    paginate_by = 6
