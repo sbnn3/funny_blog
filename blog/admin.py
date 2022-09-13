1fb959b1828b4795763f2e1e1ee81f1f67570b0a
sbnn3 <sebastian.neagu@outlook.com>
2022-09-13 14:42:02 +0000

    Added the like and unlike function on views.py, urls.py and post_detail.html files, also fixed some mistake on admin.py regarding comments

--- blog/admin.py
+++ blog/admin.py
@@ -12,8 +12,8 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
 
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
