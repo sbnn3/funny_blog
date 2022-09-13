5267c37951948377ce1e981045d0448bb2b82c07
sbnn3 <sebastian.neagu@outlook.com>
2022-09-13 16:31:29 +0000

    Updated the admin.py file with search features,prepopulated and list fields and filter list

--- guitars/admin.py
+++ guitars/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Guitars
 
 
-admin.site.register(Guitars)
+@admin.register(Guitars)
+class GuitarAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('guitar_model',)}
+    list_display = ('artist', 'guitar_model', 'price', 'created_on')
+    list_filter = ('artist', 'created_on')
+    search_fields = ['guitar_model', 'artist']
+
 
