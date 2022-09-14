0c8e48859027344f3feeabc876fd6d23235518b9
sbnn3 <sebastian.neagu@outlook.com>
2022-09-14 21:38:11 +0000

    Added the URL path for Guitars

--- funny_blog/urls.py
+++ funny_blog/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from guitars.views import GuitarsPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls'), name='blog-urls'),
     path('summernote/', include('django_summernote.urls')),
-    path('guitars/', GuitarsPage.as_view(), name='guitars'),
     path('accounts/', include('allauth.urls')),
+    path('guitars/', include('guitars.urls')),
 ]
--- guitars/urls.py
+++ guitars/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('music/', views.GuitarsPage.as_view(), name='music'),
+    path('guitars/', views.GuitarsPage.as_view(), name='guitars'),
 ]
