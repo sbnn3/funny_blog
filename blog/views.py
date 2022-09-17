f4a32d0506765375cc999586e7f1fd407e26dc8d
sbnn3 <sebastian.neagu@outlook.com>
2022-09-17 21:43:31 +0000

    Updated the blog views file in order for likes, updated post with new blogpost

--- blog/views.py
+++ blog/views.py
@@ -71,7 +71,7 @@ class BlogPostPage(View):
 class LikeBlogPost(View):
 
     def post(self, request, slug):
-        post = get_object_or_404(Post, slug=slug)
+        post = get_object_or_404(BlogPost, slug=slug)
 
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
