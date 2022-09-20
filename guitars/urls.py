6e8db10279c80f172777b178c07e7899f877c9c1
sbnn3 <sebastian.neagu@outlook.com>
2022-09-20 21:59:00 +0000

    Added the edit button to guitar-post.html file and added the url path to urls.py file on guitars section

--- guitars/urls.py
+++ guitars/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('guitars/', views.GuitarsPage.as_view(), name='guitars'),
     path('submitguitar/', views.submit_guitar, name='submit-guitar'),
+    path('edit/<slug:slug>', view.edit_guitar_post, name='edit-guitar'),
     path('<slug:slug>/', views.GuitarsPagePost.as_view(), name='guitars-post'),
     path('like/<slug:slug>', views.LikeGuitarPost.as_view(), name='like-guitars'),
 ]
