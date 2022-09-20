fa95ecb41e7fd85fb4e683da967a4c34a4b556a6
sbnn3 <sebastian.neagu@outlook.com>
2022-09-20 20:30:44 +0000

    Added a path to urls.py file in guitar section regarding submit guitar

--- guitars/urls.py
+++ guitars/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('guitars/', views.GuitarsPage.as_view(), name='guitars'),
+    path('submitguitar/', views.submit_guitar, name='submit-guitar'),
     path('<slug:slug>/', views.GuitarsPagePost.as_view(), name='guitars-post'),
     path('like/<slug:slug>', views.LikeGuitarPost.as_view(), name='like-guitars'),
 ]
