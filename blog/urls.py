0b6cb17e041d9059ea689b32ee141d268334b098
sbnn3 <sebastian.neagu@outlook.com>
2022-09-21 09:16:45 +0000

    Updated abit the about_img img section to make the image larger

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('about/', view.about_page, name='about'),
+    path('about/', views.about_page, name='about'),
     path('blog/', views.BlogPage.as_view(), name='blog'),
     path('<slug:slug>/', views.BlogPostPage.as_view(), name='blog_post'),
     path('like/<slug:slug>', views.LikeBlogPost.as_view(), name='blog_like'),
